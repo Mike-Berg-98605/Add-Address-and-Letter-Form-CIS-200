7d80bb7c6844b1da532c7835445e7504b4f63dce
Mike Berg <68578047+Mike-Berg-UL@users.noreply.github.com>
2023-02-19 14:57:50 -0500

    main

---
+++ Prog2/Prog2Form.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace UPVApp
+{
+    partial class Prog2Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.insertToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addressToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.letterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listAddressesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listParcelsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mainOutput = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.insertToolStripMenuItem,
+            this.reportToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(396, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(133, 26);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(133, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // insertToolStripMenuItem
+            // 
+            this.insertToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addressToolStripMenuItem,
+            this.letterToolStripMenuItem});
+            this.insertToolStripMenuItem.Name = "insertToolStripMenuItem";
+            this.insertToolStripMenuItem.Size = new System.Drawing.Size(59, 24);
+            this.insertToolStripMenuItem.Text = "&Insert";
+            // 
+            // addressToolStripMenuItem
+            // 
+            this.addressToolStripMenuItem.Name = "addressToolStripMenuItem";
+            this.addressToolStripMenuItem.Size = new System.Drawing.Size(145, 26);
+            this.addressToolStripMenuItem.Text = "Address";
+            this.addressToolStripMenuItem.Click += new System.EventHandler(this.addressToolStripMenuItem_Click);
+            // 
+            // letterToolStripMenuItem
+            // 
+            this.letterToolStripMenuItem.Name = "letterToolStripMenuItem";
+            this.letterToolStripMenuItem.Size = new System.Drawing.Size(145, 26);
+            this.letterToolStripMenuItem.Text = "Letter";
+            this.letterToolStripMenuItem.Click += new System.EventHandler(this.letterToolStripMenuItem_Click);
+            // 
+            // reportToolStripMenuItem
+            // 
+            this.reportToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.listAddressesToolStripMenuItem,
+            this.listParcelsToolStripMenuItem});
+            this.reportToolStripMenuItem.Name = "reportToolStripMenuItem";
+            this.reportToolStripMenuItem.Size = new System.Drawing.Size(68, 24);
+            this.reportToolStripMenuItem.Text = "&Report";
+            // 
+            // listAddressesToolStripMenuItem
+            // 
+            this.listAddressesToolStripMenuItem.Name = "listAddressesToolStripMenuItem";
+            this.listAddressesToolStripMenuItem.Size = new System.Drawing.Size(185, 26);
+            this.listAddressesToolStripMenuItem.Text = "List Addresses";
+            this.listAddressesToolStripMenuItem.Click += new System.EventHandler(this.listAddressesToolStripMenuItem_Click);
+            // 
+            // listParcelsToolStripMenuItem
+            // 
+            this.listParcelsToolStripMenuItem.Name = "listParcelsToolStripMenuItem";
+            this.listParcelsToolStripMenuItem.Size = new System.Drawing.Size(185, 26);
+            this.listParcelsToolStripMenuItem.Text = "List Parcels";
+            this.listParcelsToolStripMenuItem.Click += new System.EventHandler(this.listParcelsToolStripMenuItem_Click_1);
+            // 
+            // mainOutput
+            // 
+            this.mainOutput.Location = new System.Drawing.Point(0, 31);
+            this.mainOutput.Multiline = true;
+            this.mainOutput.Name = "mainOutput";
+            this.mainOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.mainOutput.Size = new System.Drawing.Size(396, 413);
+            this.mainOutput.TabIndex = 1;
+            // 
+            // Prog2Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 457);
+            this.Controls.Add(this.mainOutput);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Prog2Form";
+            this.Text = "Program 2";
+            this.Load += new System.EventHandler(this.Prog2Form_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem insertToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addressToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem letterToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem reportToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listAddressesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listParcelsToolStripMenuItem;
+        private System.Windows.Forms.TextBox mainOutput;
+    }
+}
+
---
+++ Prog2/Prog2Form.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//<Micael Bergamini
+//ID: 1416810
+//Program 2
+
+namespace UPVApp
+{
+    public partial class Prog2Form : Form
+    {
+        private UserParcelView upv; //creates user Parcel view instance
+        
+        public Prog2Form()
+        {
+            InitializeComponent();
+
+            upv = new UserParcelView();
+
+            //Test data
+            upv.AddAddress("  John Smith  ", "   123 Any St.   ", "  Apt. 45 ",
+                "  Louisville   ", "  KY   ", 40202);
+            upv.AddAddress("Jane Doe", "987 Main St.",
+                "Beverly Hills", "CA", 90210);
+            upv.AddAddress("James Kirk", "654 Roddenberry Way", "Suite 321",
+                "El Paso", "TX", 79901);
+            upv.AddAddress("John Crichton", "678 Pau Place", "Apt. 7",
+                "Portland", "ME", 04101);
+
+            upv.AddLetter(upv.AddressAt(0), upv.AddressAt(1), 2.19M);
+            upv.AddLetter(upv.AddressAt(2), upv.AddressAt(3), 2.99M);
+
+            
+        }       
+               
+        //Precondition: Form Loads
+        //Postcondition: info displayed
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Grading ID: 1416810\n" +
+                "CIS 200-50\n" +
+                "Program 2\n" +
+                "This program explores the creation of a simple GUI, use of " +
+                "dialog boxes, validation, and exception handling. ");
+        }
+        //precondition: form loads
+        //postcondition: form closes
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        //precondition: form is created
+        //postcondition: new address is created
+        private void addressToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AddAddressForm addressForm = new AddAddressForm();
+            DialogResult result;
+            string name;
+            string add1;
+            string add2;
+            string city;
+            string state;
+            int zip;
+
+            result = addressForm.ShowDialog();
+
+            if(result == DialogResult.OK)
+            {
+                name = addressForm.AddName;
+                add1 = addressForm.Address1;
+                add2 = addressForm.Address2;
+                city = addressForm.City;
+                state = addressForm.State;
+                zip = addressForm.Zip;
+
+                upv.AddAddress(name, add1, add2, city, state, zip);
+            }
+        }
+        //precondition: letter form exists
+        //postcondition: new letter is created
+        private void letterToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AddLetterForm letterForm = new AddLetterForm(upv.AddressList);
+            DialogResult result;            
+            decimal fixedCost;
+            
+            if (upv.AddressCount < 1)
+            {
+                MessageBox.Show("Need 1 address to create a letter");
+                return;
+            }
+
+            result = letterForm.ShowDialog();
+
+            if(result == DialogResult.OK)
+            {
+                fixedCost = letterForm.FixedCost;
+
+                upv.AddLetter(upv.AddressAt(letterForm.Origon), upv.AddressAt(letterForm.Destination), fixedCost);
+                
+            }
+
+            
+        }
+        //accidental form click
+        private void Prog2Form_Load(object sender, EventArgs e)
+        {
+            
+        }
+        //precondition: addresses exist
+        //postcondition: addresses are displayed
+        private void listAddressesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string output = "Adress List\r\n" +
+                "--------------------------\r\n";
+            foreach (Address a in upv.addresses)
+            {               
+                output += $"{a}\r\n\r\n";
+            }
+            mainOutput.Text = output;
+        }        
+        //precondition: parcels exist
+        //postcondition: parcels are displayed
+        private void listParcelsToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            string output = "Parcel List\r\n" +
+                "--------------------------\r\n";
+            foreach (Parcel p in upv.parcels)
+            {
+                output += $"{p}\r\n\r\n" +
+                    $"--------------------------------\r\n";
+            }
+            mainOutput.Text = output.ToString();
+        }
+    }
+}
